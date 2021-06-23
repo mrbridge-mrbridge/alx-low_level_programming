23abb4cbf059581090945cf9c58d462e56651619
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-06-23 02:46:46 -0700

    fghj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unist.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	
 	return (1);
 }
