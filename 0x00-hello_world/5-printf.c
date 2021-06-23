abe08ae45f3856ecd7ad1d83e2b5f12c8d5c12e6
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-06-22 19:41:21 -0700

    fdes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
