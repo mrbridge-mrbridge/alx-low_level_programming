786f9bc23144ba46a2ddc4aa17854585e243af19
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-07-07 11:29:53 -0700

    add file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string
+ * @s: the pointer input
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
