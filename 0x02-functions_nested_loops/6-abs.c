6bec4d8eb33893c227006a6e6321810aa32189ac
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-07-07 17:52:40 -0700

    add file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _abs - Convert to absolute value of an integer
+ * @n: the integer to affect
+ *
+ * Return: Return absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
