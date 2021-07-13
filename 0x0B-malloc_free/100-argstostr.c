f32efa8e83cd2f8bab8ca6f16a24717d12e3537c
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-07-13 12:12:06 -0700

    add file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,44 +3,40 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- *@ac: number of arguments
- *@av: arguments
- * Return: a pointer to a new string
+ * argstostr - function that concatenates all the arguments of your program
+ * @ac: arguments number
+ * @av: argument variables
+ * Return: Always 0.
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int j;
-	char *p = NULL;
-	int k;
-	int ext;
+	char *s;
+	int a, b, c, d;
 
-	k = 0;
-	ext = 0;
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			ext++;
-		}
-	}
-
-	p = (char *)malloc(ext + ac + 1 * sizeof(char));
-	if (p == NULL)
+	if (av == 0)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	a = 0;
+	for (b = 0; b < ac; b++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			p[k] = av[i][j];
-			k++;
-		}
-		p[k] = '\n';
-		k++;
+		for (c = 0; av[b][c] != '\0'; c++)
+			a++;
+		a++;
 	}
-	p[k] = '\0';
-	return (p);
+	a++;
+	s = malloc(a * sizeof(char));
+	if (s == 0)
+		return (NULL);
+	d = 0;
+	for (b = 0; b < ac; b++)
+	{
+		for (c = 0; av[b][c] != '\0'; c++)
+		{
+			s[d++] = av[b][c];
+		}
+		s[d++] = '\n';
+	}
+	s[d] = '\0';
+	return (s);
 }
