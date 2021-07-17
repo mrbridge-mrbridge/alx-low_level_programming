01cd18560c0924538d98cb315d9b2ea7ed36a425
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-07-16 18:59:10 -0700

    add file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		printf("Hel, World, World, Hel.\n");
+
+		return (0);
+	}
