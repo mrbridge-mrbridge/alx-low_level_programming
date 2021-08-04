fa6fb93cf642da5ce6085c24bd9c0aab74eaf8b9
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-04 11:59:10 -0700

    updated 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-	{
+	
 		printf("%d is zero\n", n);
-	} else if (n > 0)
-	{
+	if (n > 0)
+	
 		printf("%d is positive\n", n);
-	} else if (n < 0)
-	{
+	else
 		printf("%d is negative\n", n);
-	}
+	
 	return (0);
 }
