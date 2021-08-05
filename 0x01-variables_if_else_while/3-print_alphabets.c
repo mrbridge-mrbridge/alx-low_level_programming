7afa418c5ee0a9cd964e93b4cd9a702a5ee59f03
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-04 17:34:51 -0700

    update 3-print_alphabets.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@ int main(void)
         char low;
         char upp;
 
-        for (low = 'a'; low <= 'z'; low++)
-                putchar(low);
-        for (upp = 'A'; upp <= 'Z'; upp++)
-                putchar(upp);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (upp = 'A'; upp <= 'Z'; upp++)
+		putchar(upp);
 	putchar('\n');
-        return (0);
+	return (0);
 }
