9a024ab8925fb2bd15ca8891bb27df8577561fd6
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-13 09:36:37 -0700

    header changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,23 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-unsigned int lit_to_big_endian(unsigned int x);
+void printerr(char *msg, char *file, int status);
+
+void ptitle(char *title);
+void printmagic(char *buffer);
+void printclass(char *buffer);
+void printdata(char *buffer);
+void printversion(char *buffer);
+void printos(char *buffer);
+void printabversion(char *buffer);
+void err(char *msg);
 
 #endif
