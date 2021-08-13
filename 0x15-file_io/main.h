ac51e1473c20f2195036c4ae538577310d1a63d5
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-13 10:05:53 -0700

    header update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,7 @@
 #ifndef _HOLBERTON_
 #define _HOLBERTON_
+#ifndef FILE_IO
+#define FILE_IO
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,6 +9,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
